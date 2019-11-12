1fb18d8215368660e479d4fdc15c9b5a4ebfdf92
Matthew Patterson <matthewp414@gmail.com>
2019-11-12 08:13:14 -0600

    Just a start

---
+++ Dice game/UFC-Dice-Game--master/README.md
@@ -0,0 +1,1 @@
+# UFC-Dice-Game-
---
+++ Dice game/dice.js
@@ -0,0 +1,7 @@
+function roll(sides){
+	Math.ceil((Math.random( * sides)  );
+}
+function fightBout(health,ap,ep,bp,weight){
+	// How do I store my users variables in a function and pass them into the fight and change them if they arent global
+}
+
